d8ccbc18c714c1be61b221ae8f36cd5bcee22860
DEBOPAM ROY <mapobed@DEBOPAMs-MacBook-Air.local>
2023-12-07 00:19:06 +0530

    Pipe working

--- index.js
+++ index.js
@@ -3,16 +3,47 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
-
-const PORT = process.env.PORT || 4001;
+const cors = require('cors');
+const ACTION = require('./Actions');
+app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server);
-
-io.on('connection', (socket) => {
-  console.log("We are listening...", socket.id);
+const io = new Server(server,{
+  cors: {
+    origin: '*',
+  }
 });
 
+const userSocketMap= {};
+const getAllConnectedClients = (roomId) => {
+  return Array.from(io.sockets.adapter.rooms.get(roomId)|| []).map(
+    (socketId) =>{
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      }
+    });
+};
+
+io.on("connection", (socket) => {
+
+  socket.on(ACTION.JOIN,({roomId, username})=> {
+    userSocketMap[socket.id]=username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    clients.forEach(({socketId}) => {
+      io.to(socketId).emit(ACTION.JOINED,{
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
+});
+
+
+
+const PORT = process.env.PORT || 4001;
 server.listen(PORT, () => {
   console.log(`Server listening on: ${PORT}.`);
 });
